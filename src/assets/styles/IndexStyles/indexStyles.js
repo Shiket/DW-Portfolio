e50d637fb5176fe3a5fdbe4192d4953638a51af0
Shiket <shiket95@gmail.com>
2021-01-12 15:09:03 +0100

    add about me heading

--- src/assets/styles/IndexStyles/indexStyles.js
+++ src/assets/styles/IndexStyles/indexStyles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const ContentWrapper = styled.div`
   min-height: 1500px;
   margin-left:300px;
+  padding-top:8vh;
 
   @media (max-width: 1600px){
     margin-left:250px;
@@ -15,5 +16,6 @@ export const ContentWrapper = styled.div`
   }
   @media(max-width: 639px){
     margin-left: 45px;
+    padding-top:5vh;
   }
 `
--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,8 +5,69 @@ import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 export const AvatarImg = styled(Img)`
-  width:204px;
-  height:228px;
+    margin: 0 auto;
+    width:255px;
+    height:284px;
+  @media (max-width: 1921px){
+    width:204px;
+    height:228px;
+  }
+
+`
+export const MainHeading = styled.p`
+  text-align:center;
+  color: #fff;
+  padding-top:10px;
+  font-size: 26px;
+  height:58px;
+  position:relative;
+  width:70%;
+  max-width: 620px;
+  margin: 40px auto;
+  background-color: rgb(224, 64, 64);
+
+  @media(min-width:1921px){
+    max-width: 900px;
+    margin: 60px auto;
+    font-size: 29px;
+    padding-top:7px;
+  }
+
+  @media (max-width: 639px){
+    width: 100%;
+    height: 100%;
+    font-size:25px;
+    padding:10px 5px;
+  }
+  ::before{
+    position: absolute;
+    left: -53px;
+    z-index:999;
+    top: 0;
+    content: "";
+    width: 54px;
+    height: 64px;
+    background-image: url(${({ image }) => image});
+
+    @media (max-width: 639px){
+      display:none;
+    }
+  }
+
+   ::after{
+    position: absolute;
+    right: -53px;
+    z-index:999;
+    top: 0;
+    content: "";
+    width: 54px;
+    height: 64px;
+    transform: rotateY(180deg);
+    background-image: url(${({ image }) => image});
+    @media (max-width: 639px){
+      display:none;
+    }
+  }
 `
 
 export const AboutMe = () => {
@@ -16,6 +77,7 @@ export const AboutMe = () => {
     return (
       <>
         <AvatarImg fluid={data.avatar.childImageSharp.fluid} alt="avatar"/>
+        <MainHeading image={data.beforeTitle.childImageSharp.fixed.src} >Hello, my name is Dominik &#128516;</MainHeading>
       </>
     )
 }
@@ -26,6 +88,13 @@ export const query = graphql`
       childImageSharp {
         fluid(maxWidth: 406, maxHeight: 448, quality:100) {
           ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    beforeTitle: file(name: { eq: "beforeTitle" }) {
+      childImageSharp {
+        fixed(width: 54, height: 64, quality: 100) {
+          src
         }
       }
     }
