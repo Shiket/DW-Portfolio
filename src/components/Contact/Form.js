1b451d67dec0847c71ba7ff005ee2df49bfaf643
Shiket <shiket95@gmail.com>
2021-02-18 15:09:14 +0100

    add clear button

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -8,11 +8,16 @@ export const StyledInput = styled.input`
     border:2px solid #00A5A5;
     background-color: #303030;
     width:100%;
-    height:${({as})=> as ? '300px' : '50px'};
+    height:${ ({as}) => as ? '300px' : '50px'};
     font-family: 'Roboto';
     font-size:17px;
     color: #fff;
     margin-bottom:30px;
+
+
+    @media(max-width:1200px){
+        margin-bottom:20px;
+    }
 `
 
 export const StyledLabel = styled.label`
@@ -25,17 +30,47 @@ export const StyledLabel = styled.label`
 export const FormWrapper = styled.div`
     width:800px;
     margin: 100px auto 0 auto;
+
+    @media(max-width:1600px){
+        margin-top:60px
+    }
+
+    @media(max-width:1200px){
+        width:80%;
+    }
+    @media(max-width:650px){
+        margin: 60px auto 0 auto;
+        width:90%;
+    }
 `
 
 export const StyledButton = styled.button`
-    width:350px;
-    height:50px;
-    background-color: #00A5A5;
-    color: #fff;
-    border:none;
     font-family:'Roboto';
     font-weight:700;
     font-size: 21px;
+    border:${props => (props.empty ? "2px solid #00A5A5": "none" )};
+    color: #fff;
+    background-color:${props => (props.empty ? "transparent" : "#00A5A5" )};
+    width:350px;
+    height:50px;
+
+    @media(max-width:600px){
+        width:100%;
+        margin-top:15px;
+    }
+
+`
+
+export const ButtonsWrapper = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    width:100%;
+
+    @media(max-width:600px){
+        flex-direction:column;
+        justify-content:cneter;
+    }
 `
 
  export const ContactForm = () => (
@@ -55,6 +90,7 @@ export const StyledButton = styled.button`
         }}
 
         onSubmit={(values, { setSubmitting }) => {
+            console.log(values)
                 try {
                   axios.post('/.netlify/functions/sendgrid', values)
                   setSubmitting(false);
@@ -102,10 +138,15 @@ export const StyledButton = styled.button`
                 onBlur={handleBlur}
                 value={values.message}
             />
-
-            <StyledButton type="submit" disabled={isSubmitting}>
-                SUBMIT
-            </StyledButton>
+            <ButtonsWrapper>
+                <StyledButton empty="true" disabled={isSubmitting}>
+                    CLEAR
+                </StyledButton>
+                <StyledButton type="submit" disabled={isSubmitting}>
+                    SUBMIT
+                </StyledButton>
+            </ButtonsWrapper>
+            
             </form>
         )}
         </Formik>
